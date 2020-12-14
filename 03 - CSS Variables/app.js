e41b47f58843ca6476e03759f7af23b1066020db
waiscodes <b.wais@outlook.com>
2020-12-14 15:40:23 -0700

    added blur and css variables work

--- 03 - CSS Variables/app.js
+++ 03 - CSS Variables/app.js
@@ -1,8 +1,12 @@
 const input = document.querySelectorAll(".controls input");
 
 const updateHandler = (e) => {
-  const suffix = e.target.dataset.sizing;
-  console.log(suffix);
+  const suffix = e.target.dataset.sizing || "";
+
+  document.documentElement.style.setProperty(
+    `--${e.target.name}`,
+    e.target.value + suffix
+  );
 };
 
 input.forEach((x) => x.addEventListener("change", updateHandler));
