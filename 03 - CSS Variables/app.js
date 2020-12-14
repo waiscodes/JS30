af3280c1b2e86d13b9f3cd0b28da28b0e662c688
waiscodes <b.wais@outlook.com>
2020-12-14 15:31:45 -0700

    playing with dataset property

--- 03 - CSS Variables/app.js
+++ 03 - CSS Variables/app.js
@@ -1,7 +1,9 @@
 const input = document.querySelectorAll(".controls input");
 
 const updateHandler = (e) => {
-  console.log(e.target.value);
+  const suffix = e.target.dataset.sizing;
+  console.log(suffix);
 };
 
 input.forEach((x) => x.addEventListener("change", updateHandler));
+input.forEach((x) => x.addEventListener("mousemove", updateHandler));
