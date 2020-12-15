38378653eb0b5e15ee68aef9e31e9f7815cb8f83
waiscodes <b.wais@outlook.com>
2020-12-14 17:29:26 -0700

    completed map method

--- 04 - Array Cardio Day 1/app.js
+++ 04 - Array Cardio Day 1/app.js
@@ -67,10 +67,14 @@ const people = [
 const filtered = inventors.filter(
   (inventor) => inventor.year >= 1500 && inventor.year < 1600
 );
-console.table(filtered);
+// console.table(filtered);
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
+const fullNames = inventors.map(
+  (inventor) => `${inventor.first} ${inventor.last}`
+);
+// console.table(fullNames);
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
