016c06821aa137cd0e12d830065fcd0580236368
waiscodes <b.wais@outlook.com>
2020-12-16 16:08:19 -0700

    got useful console tricks. .clear, .assert, .dir and .group

---
+++ 09 - Dev Tools Domination/scripts.js
@@ -0,0 +1,25 @@
+// Regular
+
+// Interpolated
+
+// Styled
+
+// warning!
+// console.warn("message to ignore lol");
+
+// Error :|
+// console.error("yikes");
+// Info
+
+// Testing
+console.assert(5 == 5, "wrong, maths don't add up");
+
+// clearing
+
+// Viewing DOM Elements
+
+// Grouping together
+
+// counting
+
+// timing
