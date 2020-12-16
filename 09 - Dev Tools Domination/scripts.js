942521a13bb77a4d64a380015bc30e050b4268a8
waiscodes <b.wais@outlook.com>
2020-12-16 16:26:14 -0700

    got useful console tricks. .clear, .assert, .dir and .group

--- 09 - Dev Tools Domination/scripts.js
+++ 09 - Dev Tools Domination/scripts.js
@@ -13,10 +13,14 @@
 
 // Testing
 console.assert(5 == 5, "wrong, maths don't add up");
+//This is easily the most useful one here. this only runs if something is false. Better assert than console.log for that
 
 // clearing
+console.clear();
+// No more clicking buttons on the dom. Finally, I can clear it from here.
 
 // Viewing DOM Elements
+// console.dir() allows you to open it up to see what kind of methods you can use on something. This is perfect,
 
 // Grouping together
 
