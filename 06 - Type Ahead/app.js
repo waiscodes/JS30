ffa2a5e7806005766f6c8cf4fd8f0958123a0f6f
waiscodes <b.wais@outlook.com>
2020-12-16 08:05:19 -0700

    displayed cities on the dom

--- 06 - Type Ahead/app.js
+++ 06 - Type Ahead/app.js
@@ -2,9 +2,36 @@ const endpoint =
   "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
 
 const cities = [];
-
 fetch(endpoint)
   .then((blob) => blob.json())
   .then((data) => cities.push(...data));
 
-console.log(cities);
+const findMatches = (wordToMatch, cities) => {
+  return cities.filter((place) => {
+    // RegExp class allows you build on it
+    const regex = new RegExp(wordToMatch, "gi");
+    // return which ever one is true
+    return place.city.match(regex) || place.state.match(regex);
+  });
+};
+
+const displayMatches = (e) => {
+  const matchArray = findMatches(e.target.value, cities);
+  const html = matchArray
+    .map((place) => {
+      return `
+        <li>
+          <span class="name">${place.city}, ${place.state}</span>
+          <span class="population">${place.population}</span>
+        </li>
+      `;
+    })
+    .join("");
+  suggestions.innerHTML = html;
+};
+
+const searchInput = document.querySelector(".search");
+const suggestions = document.querySelector(".suggestions");
+
+searchInput.addEventListener("change", displayMatches);
+searchInput.addEventListener("keyup", displayMatches);
