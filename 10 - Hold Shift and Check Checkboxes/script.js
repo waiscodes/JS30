67eadd8055d4ec27cbd693762997ad744b9b9e93
waiscodes <b.wais@outlook.com>
2020-12-16 16:42:38 -0700

    multiple check boxes finished. Useful but not something to memorize.

---
+++ 10 - Hold Shift and Check Checkboxes/script.js
@@ -0,0 +1,24 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+const handleCheck = (e) => {
+  let inBetween = false;
+  if (e.shiftKey && e.target.checked) {
+    checkboxes.forEach((checkbox) => {
+      if (checkbox === e.target || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+  lastChecked = e.target;
+};
+
+checkboxes.forEach((checkbox) =>
+  checkbox.addEventListener("click", handleCheck)
+);
+
+// console.clear();
