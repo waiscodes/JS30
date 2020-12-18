6af9d372347d6d55c8f698e519f8c6c024fe4680
waiscodes <b.wais@outlook.com>
2020-12-18 08:10:13 -0700

    day 5 completed the javascript30 course.

---
+++ 30 - Whack A Mole/script.js
@@ -0,0 +1,49 @@
+const holes = document.querySelectorAll(".hole");
+const scoreBoard = document.querySelector(".score");
+const moles = document.querySelectorAll(".mole");
+let lastHole;
+let timeUp = false;
+let score = 0;
+
+const randomTime = (min, max) => {
+  return Math.round(Math.random() * (max - min) + min);
+};
+
+const randomHole = (holes) => {
+  const idx = Math.floor(Math.random() * holes.length);
+  const hole = holes[idx];
+  if (hole === lastHole) {
+    // Recursive solution to hitting the same number
+    return randomHole(holes);
+  }
+  lastHole = hole;
+  return hole;
+};
+
+const peep = () => {
+  const time = randomTime(200, 1000);
+  const hole = randomHole(holes);
+  hole.classList.add("up");
+  setTimeout(() => {
+    hole.classList.remove("up");
+    if (!timeUp) peep();
+  }, time);
+};
+
+const startGame = () => {
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  score = 0;
+  peep();
+  setTimeout(() => (timeUp = true), 10000);
+};
+
+const bonk = (e) => {
+  // This is a neat way of checking for bots.
+  if (!e.isTrusted) return;
+  score++;
+  e.target.parentNode.classList.remove("up");
+  scoreBoard.textContent = score;
+};
+
+moles.forEach((mole) => mole.addEventListener("click", bonk));
