fd8756ddf76e805668efd977f3e4302d41b66f08
waiscodes <b.wais@outlook.com>
2020-12-18 08:11:37 -0700

    completed the countdown timer

---
+++ 29 - Countdown Timer/scripts-START.js
@@ -0,0 +1,60 @@
+let countdown;
+const timerDisplay = document.querySelector(".display__time-left");
+const endTime = document.querySelector(".display__end-time");
+const buttons = document.querySelectorAll("[data-time]");
+
+const timer = (seconds) => {
+  clearInterval(countdown);
+
+  const now = Date.now();
+  const then = now + seconds * 1000;
+
+  displayTimeLeft(seconds);
+  displayEndTime(then);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+};
+
+const displayTimeLeft = (seconds) => {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+
+  const display = `${minutes}:${
+    remainderSeconds < 10 ? "0" : ""
+  }${remainderSeconds}`;
+
+  document.title = display;
+  timerDisplay.textContent = display;
+};
+
+const displayEndTime = (timestamp) => {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+
+  endTime.textContent = `Be Back At ${hour}:${
+    minutes < 10 ? "0" : ""
+  }${minutes}`;
+};
+
+const startTimer = (e) => {
+  const seconds = parseInt(e.target.dataset.time);
+  timer(seconds);
+};
+
+buttons.forEach((button) => button.addEventListener("click", startTimer));
+// If an element has a name attribute, you can select it directly from document without query selecting
+document.customForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const mins = e.target.minutes.value;
+
+  timer(mins * 60);
+  e.target.reset();
+});
